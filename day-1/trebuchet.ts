e8443e5aac7c10754aa3435bcdecde0ce62b4a4d
Léon Kiefer <leon.kiefer@proton.me>
2024-01-08 09:23:04 +0100

    Add return

--- day-1/trebuchet.ts
+++ day-1/trebuchet.ts
@@ -1,11 +1,16 @@
 const example2DArray = [['1abc2'], ['pqr3stu8vwx'], ['a1b2c3d4e5f'], ['treb7uchet']];
 
-const traverse2DArray = (array: string[][]): void => {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array[i].length; j++) {
-      console.log(array[i][j]);
+const traverse2DArray = (inputArray: string[][]) => {
+  const outputArray: string[][] = [];
+
+  for (let i = 0; i < inputArray.length; i++) {
+    for (let j = 0; j < inputArray[i].length; j++) {
+      console.log(inputArray[i][j]);
+      outputArray.push(inputArray[i][j])
     }
   }
+
+  return outputArray;
 };
 
 const my2DArray = traverse2DArray(example2DArray);
