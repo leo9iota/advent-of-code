faa932bd2d4f7243be993a457df815246d8c27f8
Léon Kiefer <lk9iota@gmail.com>
2024-08-06 21:50:49 +0200

    Add function to detect line endings in a file

--- advent-of-code-2023/day-1/trebuchet.ts
+++ advent-of-code-2023/day-1/trebuchet.ts
@@ -1,18 +1,23 @@
-const example2DArray = [['1abc2'], ['pqr3stu8vwx'], ['a1b2c3d4e5f'], ['treb7uchet']];
+import { promises as fs } from 'node:fs';
 
-const traverse2DArray = (inputArray: string[][]) => {
-  const outputArray: string[][] = [];
+async function detectLineEndings(filePath: string) {
+    try {
+        const data = await fs.readFile(filePath);
+        const content = data.toString('utf-8');
 
-  for (let i = 0; i < inputArray.length; i++) {
-    for (let j = 0; j < inputArray[i].length; j++) {
-      console.log(inputArray[i][j]);
-      outputArray.push([inputArray[i][j]]);
+        if (content.includes('\r\n')) {
+            console.log('The file uses CRLF (Windows) line endings.');
+            console.log(data.toString());
+        } else if (content.includes('\n')) {
+            console.log('The file uses LF (Unix) line endings.');
+            console.log(data.toString());
+        } else {
+            console.log('The file does no   t contain any standard line endings.');
+            console.log(data.toString());
+        }
+    } catch (err) {
+        console.error('Error reading file:', err);
     }
-  }
+}
 
-  return outputArray;
-};
-
-const my2DArray = traverse2DArray(example2DArray);
-
-console.log(my2DArray);
+detectLineEndings('./advent-of-code-2023/day-1/input.txt');
