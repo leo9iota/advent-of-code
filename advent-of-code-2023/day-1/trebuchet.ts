79bb29849f071d6f1a31527ab5de2261bf37187a
Léon Kiefer <lk9iota@gmail.com>
2024-08-12 16:18:48 +0200

    Add regex pattern

--- advent-of-code-2023/day-1/trebuchet.ts
+++ advent-of-code-2023/day-1/trebuchet.ts
@@ -6,27 +6,34 @@ async function computeCalibrationValues(filePath: string) {
     try {
         const data: string = await fs.readFile(filePath, 'utf-8'); // Read from file using UTF-8 format
         const lines: string[] = data.split('\n'); // Split lines into separate strings
+        const regexPattern = /\b(one|two|three|four|five|six|seven|eight|nine)\b/;
         let sumCalibrationValues: number = 0; // Initialize sum variable
 
-        for (const line of lines) { // Loop over the separate strings
+        // Loop over the separate strings
+        for (const line of lines) {
             let firstDigit: number | null = null; // Initialize first digit variable
             let lastDigit: number | null = null; // Initialize last digit variable
 
-            for (let i = 0; i < line.length; i++) { // Loop over the string by its length
+            for (let i = 0; i < line.length; i++) {
+                // Loop over the string by its length
                 const number: number = parseInt(line[i]); // Parses the single character to an integer
 
-                if (!isNaN(number)) { // Check if the parsed integer is a number
-                    if (firstDigit === null) { // Check if the first digit variable is still unassigned
+                if (!isNaN(number)) {
+                    // Check if the parsed integer is a number
+                    if (firstDigit === null) {
+                        // Check if the first digit variable is still unassigned
                         firstDigit = number; // Assign first digit to the number in the string
                     }
                     lastDigit = number; // Assign last digit to the number in the string
                 }
             }
 
-            if (firstDigit !== null && lastDigit !== null) { // Check if first and last digit have been assigned
+            if (firstDigit !== null && lastDigit !== null) {
+                // Check if first and last digit have been assigned
                 const twoDigitNumber = firstDigit * 10 + lastDigit; // Add them together (Note: the first digit is multiplied by 10 to form a two-digit number)
                 sumCalibrationValues += twoDigitNumber; // Add to the total sum
-            } else if (firstDigit !== null) { // Check if theres only one digit
+            } else if (firstDigit !== null) {
+                // Check if theres only one digit
                 const twoDigitNumber = firstDigit * 10 + firstDigit; // Add them together (Note: the first digit is multiplied by 10 to form a two-digit number)
                 sumCalibrationValues += twoDigitNumber; // Add to the total sum
             }
