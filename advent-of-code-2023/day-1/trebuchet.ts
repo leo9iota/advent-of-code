f2f3f5b84f0a76699625e1577776ec0619d137dd
Léon Kiefer <lk9iota@gmail.com>
2024-08-12 13:37:38 +0200

    Add implementation for AOC 2023 Day 1 Part 1

--- advent-of-code-2023/day-1/trebuchet.ts
+++ advent-of-code-2023/day-1/trebuchet.ts
@@ -1,14 +1,38 @@
 import { promises as fs } from 'node:fs';
 
+// const exampleInput = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet'];
+
 async function computeCalibrationValues(filePath: string) {
     try {
-        const data = await fs.readFile(filePath, 'utf-8');
-        const lines = data.split('\n')
-        let sumCalibrationValues = 0;
+        const data: string = await fs.readFile(filePath, 'utf-8'); // Read from file using UTF-8 format
+        const lines: string[] = data.split('\n'); // Split lines into separate strings
+        let sumCalibrationValues: number = 0; // Initialize sum variable
 
-        for (const line of lines) {
-            
+        for (const line of lines) { // Loop over the separate strings
+            let firstDigit: number | null = null; // Initialize first digit variable
+            let lastDigit: number | null = null; // Initialize last digit variable
+
+            for (let i = 0; i < line.length; i++) { // Loop over the string by its length
+                const number: number = parseInt(line[i]); // Parses the single character to an integer
+
+                if (!isNaN(number)) { // Check if the parsed integer is a number
+                    if (firstDigit === null) { // Check if the first digit variable is still unassigned
+                        firstDigit = number; // Assign first digit to the number in the string
+                    }
+                    lastDigit = number; // Assign last digit to the number in the string
+                }
+            }
+
+            if (firstDigit !== null && lastDigit !== null) { // Check if first and last digit have been assigned
+                const twoDigitNumber = firstDigit * 10 + lastDigit; // Add them together (Note: the first digit is multiplied by 10 to form a two-digit number)
+                sumCalibrationValues += twoDigitNumber; // Add to the total sum
+            } else if (firstDigit !== null) { // Check if theres only one digit
+                const twoDigitNumber = firstDigit * 10 + firstDigit; // Add them together (Note: the first digit is multiplied by 10 to form a two-digit number)
+                sumCalibrationValues += twoDigitNumber; // Add to the total sum
+            }
         }
+
+        console.log(`Sum of calibration values: ${sumCalibrationValues}`);
     } catch (error) {
         console.error(error);
     }
