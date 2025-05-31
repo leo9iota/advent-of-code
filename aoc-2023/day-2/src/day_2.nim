112e5de81c362a1a5971ddedda2a6fbc56fa27f5
leo9iota <leo9iota@gmail.com>
2025-05-31 17:43:05 +0200

    Add implementation for game parsing logic

--- aoc-2023/day-2/src/day_2.nim
+++ aoc-2023/day-2/src/day_2.nim
@@ -1,5 +1,32 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import std/[strutils, parseutils]
 
-when isMainModule:
-    echo("Hello, Nim!")
+type
+    ObservedCube* = object
+        count*: int
+        color*: string
+
+    ObservedSet* = seq[ObservedCube]
+
+    Game* = object
+        id*: int
+        rounds*: seq[ObservedSet]
+
+proc parseObservedCube*(token: string): ObservedCube =
+    var i = 0
+    result.count = parseInt(token, i)
+    result.color = token[i .. ^1].strip.toLowerAscii()
+
+proc parseObservedSet*(line: string): ObservedSet =
+    for part in line.split(','):
+        let token = part.strip
+        if token.len > 0:
+            result.add parseObservedCube(token)
+
+proc parseGameLine*(line: string): Game =
+    let (left, right) = line.split(":", maxsplit = 1)
+    result.id = left.splitWhitespace()[1].parseInt
+
+    for segment in right.split(';'):
+        let trimmed = segment.strip
+        if trimmed.len > 0:
+            result.rounds.add parseObservedSet(trimmed)
