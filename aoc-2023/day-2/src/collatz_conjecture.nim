e06dcfb6a286b4abfd419473b22dbaf8ecb32334
leo9iota <leo9iota@gmail.com>
2025-06-02 16:35:04 +0200

    Implement Collatz Conjecture logic and user input handling

--- aoc-2023/day-2/src/collatz_conjecture.nim
+++ aoc-2023/day-2/src/collatz_conjecture.nim
@@ -2,3 +2,19 @@
 # Repeat this procedure until you arrive at one.
 # E.g. 5 → odd → 3*5 + 1 = 16 → even → 16 / 2 = 8 → even → 4 → 2 → 1 → end!
 
+proc getUserInput(): string =
+    stdout.write "Enter an integer: "
+    let userInput: string = readLine(stdin)
+    echo "User chose: ", userInput, " as input."
+
+proc collatzConjecture(userInput: string): int =
+    if userInput % 2 != 0:
+        userInput * 3 + 1
+        echo userInput
+    else:
+        userInput / 2
+        echo userInput
+
+when isMainModule:
+    let userInput = getUserInput()
+    let collatzConjecture = collatzConjecture(userInput)
