f6bd5d592eb5b276b59d9303ee2d5a218f71f7d7
leo9iota <leo9iota@gmail.com>
2025-06-02 13:00:08 +0200

    Add `collatz_conjecture.nim` exercise

---
+++ aoc-2023/day-2/src/collatz_conjecture.nim
@@ -0,0 +1,4 @@
+# First pick a number. If it is odd, multiply it by three and add one; if it is even, divide it by two.
+# Repeat this procedure until you arrive at one.
+# E.g. 5 → odd → 3*5 + 1 = 16 → even → 16 / 2 = 8 → even → 4 → 2 → 1 → end!
+
