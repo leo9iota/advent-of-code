42a1078916d96e19663d9487ea12f3c1715887f5
leo9iota <leo9iota@gmail.com>
2026-02-11 22:31:29 +0100

    feat(2025): solve day 1 part 1 problem

--- 2025/day01/secret_entrance.py
+++ 2025/day01/secret_entrance.py
@@ -1,3 +1,6 @@
+from typing import Final
+
+
 def parse_input(path: str) -> tuple[list[str], list[int]]:
     directions: list[str] = []  # init list for storing directions
     steps: list[int] = []  # init list for storing steps
@@ -5,26 +8,44 @@ def parse_input(path: str) -> tuple[list[str], list[int]]:
     with open(file=path, mode="r") as file:  # safely open file
         for line in file:
             clean_line: str = line.strip()  # strip whitespace and invisible '\n'
+            if not clean_line:
+                continue  # skip empty lines
             directions.append(clean_line[:1])  # extract the first char
             steps.append(int(clean_line[1:]))  # extract everything after the first char
 
     return directions, steps
 
 
-def solve() -> None:
-    direction, step = parse_input(path="./example-input.txt")
+def solve_part_1() -> None:
+    # range of the circular-shaped dial, it can go from 0 to 99, which is 100 steps
+    DIAL_RANGE: Final = 100
+    START_POS: Final = 50  # Advent of Code 2025 Day 1 starts the dial at 50
 
-    current_pos = 0
+    # call input parser and catch return in a tuple
+    directions, steps = parse_input(path="./secret-entrance-input.txt")
 
-    for d, s in zip(direction, step, strict=False):
+    # init and declare current dial pos (0-99) and the counter for landing on zero
+    current_pos: int = START_POS
+    zero_landings: int = 0
+
+    # iterate through instructions using zip
+    for d, s in zip(directions, steps, strict=True):
+        # calculate move based on direction
         if d == "R":
             current_pos += s
-        elif d == "L":
+        else:
             current_pos -= s
 
-        current_pos %= 100
+        # use modulo to wrap the dial around the 0-99 range
+        current_pos %= DIAL_RANGE
 
-    print(f"Final Dial Position: {current_pos}")
+        # increment counter if the dial stopped exactly on zero
+        if current_pos == 0:
+            zero_landings += 1
+
+    print(f"Final dial position: {current_pos}")
+    print(f"Total times landed on zero: {zero_landings}")
 
 
-solve()
+if __name__ == "__main__":
+    solve_part_1()
--- 2025/day01/test.py
+++
@@ -1,8 +0,0 @@
-a, b = 10, 3
-print(a % b)  # Output: 1
-
-c, d = -10, 3
-print(c % d)  # Output: 2
-
-e, f = 10.5, 2
-print(e % f)  # Output: 0.5
